e1413a7e4846fb85359e889803bed73707e7fab1
jonathan.moraes.gft <jonathan.moraes.gft@nubank.com.br>
2024-11-25 09:16:46 -0300

    Fixing name of keystore

--- web-server/nginx/init.sh
+++ web-server/nginx/init.sh
@@ -3,7 +3,7 @@
 set -eu
 
 mkdir -p /etc/nginx/certs
-aws s3 cp --region us-west-2 s3://nu-keysets-data-staging/certificates/pri/global/keystore.p12-2022-2023 /etc/nginx/certs
+aws s3 cp --region us-west-2 s3://nu-keysets-data-staging/certificates/pri/global/keystore.p12 /etc/nginx/certs
 cd /etc/nginx/certs
 openssl pkcs12 -in keystore.p12 -passin pass:nubankp12 -nocerts -nodes | sed -n '/BEGIN/,$p' > nubank_key.pem
 openssl pkcs12 -in keystore.p12 -passin pass:nubankp12 -nokeys > nubank_cert.pem
