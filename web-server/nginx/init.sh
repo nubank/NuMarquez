5162f9db754dfe58fc67450b916b7e3ccad115d4
jonathan.moraes.gft <jonathan.moraes.gft@nubank.com.br>
2024-12-03 18:08:35 -0300

    Configuring files and testing image generation

--- web-server/nginx/init.sh
+++ web-server/nginx/init.sh
@@ -3,7 +3,7 @@
 set -eu
 
 mkdir -p /etc/nginx/certs
-aws s3 cp --region us-east-1 s3://nu-keysets-br-${NU_ENV}/certificates/pri/global/keystore.p12 /etc/nginx/certs
+aws s3 cp --region us-east-1 s3://nu-keysets-data-staging/certificates/pri/staging-marquez-nginx-proxy/acme/keystore.p12 /etc/nginx/certs
 cd /etc/nginx/certs
 openssl pkcs12 -in keystore.p12 -passin pass:nubankp12 -nocerts -nodes | sed -n '/BEGIN/,$p' > nubank_key.pem
 openssl pkcs12 -in keystore.p12 -passin pass:nubankp12 -nokeys > nubank_cert.pem
