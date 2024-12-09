706fd87b354dddf728776478b3e54d9ddaf2eb91
jonathan.moraes.gft <jonathan.moraes.gft@nubank.com.br>
2024-12-09 11:12:10 -0300

    Updating vouch image

--- web-server/vouch-proxy/init.sh
+++ web-server/vouch-proxy/init.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-export OAUTH_CLIENT_ID=$(aws s3 cp ${OAUTH_CLIENT_ID_S3_LOCATION} -)
-export OAUTH_CLIENT_SECRET=$(aws s3 cp ${OAUTH_CLIENT_SECRET_S3_LOCATION} -)
+# export OAUTH_CLIENT_ID=$(aws s3 cp ${OAUTH_CLIENT_ID_S3_LOCATION} -)
+# export OAUTH_CLIENT_SECRET=$(aws s3 cp ${OAUTH_CLIENT_SECRET_S3_LOCATION} -)
+
+export OAUTH_CLIENT_ID=0oa1zfdpg80bKlXcq0h8
+export OAUTH_CLIENT_SECRET=dwqew26OdAfXMaEfRHEVnVP05SmXbfwdtkOeOtSBQ6Z-vpyexeKV9MnsTgLDY8Bo
 
 /vouch-proxy
