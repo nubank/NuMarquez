b5411c6c66a6debc2b1b052278075385047cd78c
Jonathan P Moraes <jonathan.moraes.gft@nubank.com.br>
2025-02-13 16:20:00 -0300

    Adding new classes

--- web/src/components/ga4.tsx
+++ web/src/components/ga4.tsx
@@ -1,7 +1,7 @@
 import ReactGA from 'react-ga4';
 
 const initializeGA = () => {
-    ReactGA.initialize('G-QF2RHX3HRJ');
+    ReactGA.initialize('G-497622C5B5');
   };
   
   const trackPageView = () => {
