ada4f8c176bcc389ae123df69ac08d53471f7ad4
Jonathan P Moraes <jonathan.moraes.gft@nubank.com.br>
2025-04-30 11:35:35 -0300

    Adjusting testing environment

--- api/src/main/java/marquez/MarquezConfig.java
+++ api/src/main/java/marquez/MarquezConfig.java
@@ -3,63 +3,64 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package marquez;
+ package marquez;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.collect.ImmutableSet;
-import io.dropwizard.Configuration;
-import io.dropwizard.db.DataSourceFactory;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import marquez.api.filter.exclusions.ExclusionsConfig;
-import marquez.db.FlywayFactory;
-import marquez.graphql.GraphqlConfig;
-import marquez.jobs.DbRetentionConfig;
-import marquez.search.SearchConfig;
-import marquez.service.models.Tag;
-import marquez.tracing.SentryConfig;
-
-/** Configuration for {@code Marquez}. */
-@NoArgsConstructor
-public class MarquezConfig extends Configuration {
-  private static final boolean DEFAULT_MIGRATE_ON_STARTUP = true;
-  private static final ImmutableSet<Tag> DEFAULT_TAGS = ImmutableSet.of();
-
-  @Getter private boolean migrateOnStartup = DEFAULT_MIGRATE_ON_STARTUP;
-  @Getter private ImmutableSet<Tag> tags = DEFAULT_TAGS;
-
-  @Getter
-  @JsonProperty("db")
-  private final DataSourceFactory dataSourceFactory = new DataSourceFactory();
-
-  @Getter
-  @JsonProperty("flyway")
-  private final FlywayFactory flywayFactory = new FlywayFactory();
-
-  @Getter
-  @JsonProperty("graphql")
-  private final GraphqlConfig graphql = new GraphqlConfig();
-
-  @Getter
-  @JsonProperty("sentry")
-  private final SentryConfig sentry = new SentryConfig();
-
-  @Getter
-  @JsonProperty("search")
-  private final SearchConfig searchConfig = new SearchConfig();
-
-  @Getter
-  @Setter
-  @JsonProperty("dbRetention")
-  private DbRetentionConfig dbRetention; // OPTIONAL
-
-  @Getter
-  @JsonProperty("exclude")
-  private ExclusionsConfig exclude = new ExclusionsConfig();
-
-  /** Returns {@code true} if a data retention policy has been configured. */
-  public boolean hasDbRetentionPolicy() {
-    return (dbRetention != null);
-  }
-}
+ import com.fasterxml.jackson.annotation.JsonProperty;
+ import com.google.common.collect.ImmutableSet;
+ import io.dropwizard.Configuration;
+ import io.dropwizard.db.DataSourceFactory;
+ import lombok.Getter;
+ import lombok.NoArgsConstructor;
+ import lombok.Setter;
+ import marquez.api.filter.exclusions.ExclusionsConfig;
+ import marquez.db.FlywayFactory;
+ import marquez.graphql.GraphqlConfig;
+ import marquez.jobs.DbRetentionConfig;
+ import marquez.search.SearchConfig;
+ import marquez.service.models.Tag;
+ import marquez.tracing.SentryConfig;
+ 
+ /** Configuration for {@code Marquez}. */
+ @NoArgsConstructor
+ public class MarquezConfig extends Configuration {
+   private static final boolean DEFAULT_MIGRATE_ON_STARTUP = true;
+   private static final ImmutableSet<Tag> DEFAULT_TAGS = ImmutableSet.of();
+ 
+   @Getter private boolean migrateOnStartup = DEFAULT_MIGRATE_ON_STARTUP;
+   @Getter private ImmutableSet<Tag> tags = DEFAULT_TAGS;
+ 
+   @Getter
+   @JsonProperty("db")
+   private final DataSourceFactory dataSourceFactory = new DataSourceFactory();
+ 
+   @Getter
+   @JsonProperty("flyway")
+   private final FlywayFactory flywayFactory = new FlywayFactory();
+ 
+   @Getter
+   @JsonProperty("graphql")
+   private final GraphqlConfig graphql = new GraphqlConfig();
+ 
+   @Getter
+   @JsonProperty("sentry")
+   private final SentryConfig sentry = new SentryConfig();
+ 
+   @Getter
+   @JsonProperty("search")
+   private final SearchConfig searchConfig = new SearchConfig();
+ 
+   @Getter
+   @Setter
+   @JsonProperty("dbRetention")
+   private DbRetentionConfig dbRetention; // OPTIONAL
+ 
+   @Getter
+   @JsonProperty("exclude")
+   private ExclusionsConfig exclude = new ExclusionsConfig();
+ 
+   /** Returns {@code true} if a data retention policy has been configured. */
+   public boolean hasDbRetentionPolicy() {
+     return (dbRetention != null);
+   }
+ }
+ 
--- api/src/main/java/marquez/db/TagDao.java
+++ api/src/main/java/marquez/db/TagDao.java
@@ -3,53 +3,54 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package marquez.db;
+ package marquez.db;
 
-import java.time.Instant;
-import java.util.Set;
-import java.util.UUID;
-import marquez.db.mappers.TagMapper;
-import marquez.db.mappers.TagRowMapper;
-import marquez.db.models.TagRow;
-import marquez.service.models.Tag;
-import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
-import org.jdbi.v3.sqlobject.statement.SqlQuery;
-
-@RegisterRowMapper(TagRowMapper.class)
-@RegisterRowMapper(TagMapper.class)
-public interface TagDao {
-  @SqlQuery(
-      "INSERT INTO tags (uuid, created_at, updated_at, name) "
-          + "VALUES (:uuid, :updatedAt, :updatedAt, :name) "
-          + "ON CONFLICT(name) DO UPDATE SET updated_at = EXCLUDED.updated_at "
-          + "RETURNING *")
-  TagRow upsert(UUID uuid, Instant updatedAt, String name);
-
-  @SqlQuery(
-      "INSERT INTO tags (uuid, created_at, updated_at, name, description) "
-          + "VALUES (:uuid, :updatedAt, :updatedAt, :name, :description) "
-          + "ON CONFLICT(name) DO UPDATE SET "
-          + "updated_at = EXCLUDED.updated_at, "
-          + "description = EXCLUDED.description "
-          + "RETURNING *")
-  TagRow upsert(UUID uuid, Instant updatedAt, String name, String description);
-
-  default Tag upsert(Tag tag) {
-    Instant now = Instant.now();
-    if (tag.getDescription().isPresent()) {
-      upsert(UUID.randomUUID(), now, tag.getName().getValue(), tag.getDescription().get());
-    } else {
-      upsert(UUID.randomUUID(), now, tag.getName().getValue());
-    }
-    return find(tag.getName().getValue());
-  }
-
-  @SqlQuery("SELECT EXISTS (SELECT 1 FROM tags WHERE name = :name)")
-  boolean exists(String name);
-
-  @SqlQuery("SELECT * FROM tags WHERE name = :name")
-  Tag find(String name);
-
-  @SqlQuery("SELECT * FROM tags ORDER BY name LIMIT :limit OFFSET :offset")
-  Set<Tag> findAll(int limit, int offset);
-}
+ import java.time.Instant;
+ import java.util.Set;
+ import java.util.UUID;
+ import marquez.db.mappers.TagMapper;
+ import marquez.db.mappers.TagRowMapper;
+ import marquez.db.models.TagRow;
+ import marquez.service.models.Tag;
+ import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
+ import org.jdbi.v3.sqlobject.statement.SqlQuery;
+ 
+ @RegisterRowMapper(TagRowMapper.class)
+ @RegisterRowMapper(TagMapper.class)
+ public interface TagDao {
+   @SqlQuery(
+       "INSERT INTO tags (uuid, created_at, updated_at, name) "
+           + "VALUES (:uuid, :updatedAt, :updatedAt, :name) "
+           + "ON CONFLICT(name) DO UPDATE SET updated_at = EXCLUDED.updated_at "
+           + "RETURNING *")
+   TagRow upsert(UUID uuid, Instant updatedAt, String name);
+ 
+   @SqlQuery(
+       "INSERT INTO tags (uuid, created_at, updated_at, name, description) "
+           + "VALUES (:uuid, :updatedAt, :updatedAt, :name, :description) "
+           + "ON CONFLICT(name) DO UPDATE SET "
+           + "updated_at = EXCLUDED.updated_at, "
+           + "description = EXCLUDED.description "
+           + "RETURNING *")
+   TagRow upsert(UUID uuid, Instant updatedAt, String name, String description);
+ 
+   default Tag upsert(Tag tag) {
+     Instant now = Instant.now();
+     if (tag.getDescription().isPresent()) {
+       upsert(UUID.randomUUID(), now, tag.getName().getValue(), tag.getDescription().get());
+     } else {
+       upsert(UUID.randomUUID(), now, tag.getName().getValue());
+     }
+     return find(tag.getName().getValue());
+   }
+ 
+   @SqlQuery("SELECT EXISTS (SELECT 1 FROM tags WHERE name = :name)")
+   boolean exists(String name);
+ 
+   @SqlQuery("SELECT * FROM tags WHERE name = :name")
+   Tag find(String name);
+ 
+   @SqlQuery("SELECT * FROM tags ORDER BY name LIMIT :limit OFFSET :offset")
+   Set<Tag> findAll(int limit, int offset);
+ }
+ 
