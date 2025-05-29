ec1cb2bee506c58ab25b392aa48e9d30ed4f268e
Jonathan P Moraes <jonathan.moraes.gft@nubank.com.br>
2025-05-29 10:50:48 -0300

    Implementing complex mapper

--- api/src/main/java/marquez/db/mappers/ComplexDatasetFieldPairMapper.java
+++ api/src/main/java/marquez/db/mappers/ComplexDatasetFieldPairMapper.java
@@ -17,14 +17,17 @@ public final class ComplexDatasetFieldPairMapper implements RowMapper<Pair<Pair<
     @Override
     public Pair<Pair<String, Pair<String, String>>, UUID> map(@NonNull ResultSet results, @NonNull StatementContext context)
             throws SQLException {
+        // Map the columns to the complex Pair structure
         String namespaceName = results.getString("namespace_name");
         String datasetName = results.getString("dataset_name");
         String fieldName = results.getString("name");
         UUID uuid = results.getObject("uuid", UUID.class);
 
+        // Construct the nested Pair structure
         Pair<String, String> datasetPair = Pair.of(datasetName, fieldName);
         Pair<String, Pair<String, String>> identifierPair = Pair.of(namespaceName, datasetPair);
         
+        // Return the final Pair with UUID
         return Pair.of(identifierPair, uuid);
     }
 } 
